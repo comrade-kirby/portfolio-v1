6dd79e84703add01505a80b364bd22a800093c39
Max Oppenheimer <maxoppenheimer1993@gmail.com>
2017-12-06 14:20:51 -0500

    scroll on name click

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
 $(document).ready(function () {
-    $('.parallax').parallax()
-    console.log('hi')
-});
+  $('.parallax').parallax()
+
+  $('h1 span').click(function () {
+    // let position = $(this).position()
+    // let clone = $(this).clone()
+    // $(this).addClass('invisible-letter')
+    // if ($(this).prev().length !== 0) {
+    //   $(this).prev().after(clone)
+    // } else {
+    //   $(this).next().before(clone)
+    // }
+    // clone.addClass('fixed-letter')
+    // clone.css(position)
+    $('html, body').animate({
+      scrollTop: $(".about").offset().top
+    }, 1000);
+  })
+})
